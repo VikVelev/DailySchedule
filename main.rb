5e0322b26e8384a8848f34812191766c6a61695f
VikVelev <viktorvelev8@gmail.com>
2017-10-07 17:03:53 +0300

    bugfixes #2

--- main.rb
+++ main.rb
@@ -3,13 +3,13 @@ require './engine.rb'
 
 def main()
     currentDayWord = Time.now.strftime("%A")
+
     if (ARGV[0] == "help")
         Help()
-    elsif 
+    else
         if (ARGV[0] != nil)
             currentDayWord = ARGV[0]
         end
-
         if (CheckForSchedule(currentDayWord))
             PrintSchedule(currentDayWord)
         else    
